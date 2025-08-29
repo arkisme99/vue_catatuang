0f3dbb0aa608344b8c709019f83353e219fba307
Achmad Rizky <arkisme9997@gmail.com>
2025-08-29 17:40:38 +0700

    fix: delete unused file

--- src/lib/toLogin.ts
+++
@@ -1,6 +0,0 @@
-import { useRouter } from "vue-router";
-
-export const toLogin = async (): Promise<void> => {
-  const router = useRouter();
-  await router.push({ path: "/login" });
-};
--- src/stores/auth.ts
+++ src/stores/auth.ts
@@ -3,40 +3,28 @@ import { ProfileUser } from "@/model/UserModel";
 import { defineStore } from "pinia";
 import { ref, watch } from "vue";
 import { AuthService } from "@/services/AuthService";
-// import { useLogoutMain } from "@/composable/useLogoutMain";
-// import { useLogout } from "@/composable/useLogout";
 
 export const useAuthStore = defineStore("auth", () => {
   const PROFILE_KEY = "profile-user";
   const AUTH_TOKEN = "session-token";
 
-  // const authToken = ref<string | null>(null);
   const authToken = useLocalStorage<string | null>(AUTH_TOKEN, null);
   const authProfile = useLocalStorage<ProfileUser | null>(PROFILE_KEY, null);
   const isTokenValid = ref<boolean>(false);
-  // const uLogout = useLogout();
-  // const uLogoutMain = useLogoutMain();
-  // const { handleLogout } = uLogout;
-  // const { handleLogoutOnMain } = uLogoutMain;
 
   // Watch token changes, auto logout jika token diubah manual
   watch(authToken, async (newVal, oldVal) => {
-    console.warn("Masuk ke watch");
     if (oldVal && newVal !== oldVal) {
-      console.warn("Token di localStorage diubah manual!");
-      // await handleLogout();
-      // await AuthService.logout();
+      // console.warn("Token di localStorage diubah manual!");
       logout();
     }
   });
 
   async function checkToken(): Promise<void> {
     if (!authToken.value) {
-      // await handleLogout();
       await AuthService.logout();
       logout();
     }
-    // console.log(`cekToken start`);
     try {
       const response = await AuthService.getProfile();
 
@@ -44,10 +32,8 @@ export const useAuthStore = defineStore("auth", () => {
       if (!response.ok) {
         return;
       }
-      // console.log(`Token: ${responseBody.data}`);
       setData(responseBody.data);
     } catch (e) {
-      // await handleLogout();
       await AuthService.logout();
       logout();
       throw e;
@@ -57,7 +43,6 @@ export const useAuthStore = defineStore("auth", () => {
     if (!authToken.value) {
       logout();
     }
-    // console.log(`cekToken start`);
     try {
       const response = await AuthService.getProfile();
 
@@ -65,7 +50,6 @@ export const useAuthStore = defineStore("auth", () => {
       if (!response.ok) {
         return;
       }
-      // console.log(`Token: ${responseBody.data}`);
       setData(responseBody.data);
     } catch (e) {
       logout();
@@ -74,7 +58,6 @@ export const useAuthStore = defineStore("auth", () => {
   }
 
   function setData(data: ProfileUser): void {
-    // console.log(`setData: ${JSON.stringify(data)}`);
     if (data.token) {
       authToken.value = data.token;
     }
